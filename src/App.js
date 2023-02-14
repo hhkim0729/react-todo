02fa996538b83fc82f410d3e2134a05001dfadef
hhkim0729 <heehkim@student.42seoul.kr>
2023-02-14 16:14:11 +0900

    Add adding item to list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ItemList from './components/ItemList';
 
 function App() {
+  const id = useRef(3);
   const [items, setItems] = useState([
     {
       id: 0,
@@ -22,11 +23,19 @@ function App() {
       isActive: true,
     },
   ]);
+
+  const onItemAdd = (title) => {
+    setItems((items) => [
+      ...items,
+      { id: id.current++, title, isActive: true },
+    ]);
+  };
+
   return (
     <div className='App'>
       <Header />
       <ItemList items={items} />
-      <Footer />
+      <Footer onItemAdd={onItemAdd} />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Footer() {
+export default function Footer({ onItemAdd }) {
+  const [title, setTitle] = useState('');
+
+  const onChangeInput = ({ target }) => {
+    setTitle(target.value);
+  };
+
+  const addItem = () => {
+    onItemAdd(title);
+    setTitle('');
+  };
+
+  const onKeyDownInput = (e) => {
+    if (e.key === 'Enter') addItem();
+  };
+
   return (
     <footer>
-      <input type='text' placeholder='Add Todo' />
-      <button>Add</button>
+      <input
+        type='text'
+        name='todo'
+        onChange={onChangeInput}
+        onKeyDown={onKeyDownInput}
+        value={title}
+        placeholder='Add Todo'
+      />
+      <button onClick={addItem}>Add</button>
     </footer>
   );
 }
