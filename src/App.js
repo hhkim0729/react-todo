17b60bda3318aee060c6274d4f879ca9c4e17645
hhkim0729 <heehkim@student.42seoul.kr>
2023-02-16 18:01:17 +0900

    Add filter active style

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Header onClickFilter={onClickFilter} />
+      <Header filter={filter} onClickFilter={onClickFilter} />
       <ItemList
         items={items}
         filter={filter}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import { Filter } from '../types';
 import styles from './Header.module.css';
 
-export default function Header({ onClickFilter }) {
+export default function Header({ filter, onClickFilter }) {
   const handleClick = ({ target }) => {
     if (target.nodeName === 'BUTTON') {
       onClickFilter(target.innerText);
@@ -16,11 +16,27 @@ export default function Header({ onClickFilter }) {
         <MdLightMode />
       </button>
       <div className={styles.filters}>
-        <button className={`${styles.button} ${styles.active}`}>
+        <button
+          className={`${styles.button} ${
+            filter === Filter.all ? styles.active : ''
+          }`}
+        >
           {Filter.all}
         </button>
-        <button className={styles.button}>{Filter.active}</button>
-        <button className={styles.button}>{Filter.completed}</button>
+        <button
+          className={`${styles.button} ${
+            filter === Filter.active ? styles.active : ''
+          }`}
+        >
+          {Filter.active}
+        </button>
+        <button
+          className={`${styles.button} ${
+            filter === Filter.completed ? styles.active : ''
+          }`}
+        >
+          {Filter.completed}
+        </button>
       </div>
     </header>
   );
